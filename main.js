c01c7ec794f86d5fc1512b81a905328df03ed6cf
Tim Pierce <tim@Tims-MacBook-Pro-4.local>
2023-04-04 22:52:21 -0600

    ADD: js functionality. Drew canvas, piece creation + movement + border hit detection

---
+++ main.js
@@ -0,0 +1,98 @@
+//getting html elements
+const game = document.getElementById('game'); 
+const ctx = game.getContext('2d'); 
+
+//setting up the pieces 
+let piece1; 
+let piece2; 
+let piece3; 
+let piece4; 
+
+//main event listener
+window.addEventListener('DOMContentLoaded', function() {
+
+    piece1 = new tetrimino(200, 10, 30, 30); 
+
+    const runGame = setInterval(gameLoop, 60); 
+    const pieceFunction = setInterval(scrollPiece, 60); 
+    detectBorder(); 
+
+})
+
+game.setAttribute('height', getComputedStyle(game)['height']); 
+game.setAttribute('width', getComputedStyle(game)['width']); 
+
+//random color operations 
+const colorIndex = ['#c92a2a', 'white']; 
+let randomIndex = Math.floor(Math.random() * (colorIndex.length)); 
+let randomColor = colorIndex[randomIndex]; 
+
+console.log(randomColor); 
+
+class tetrimino {
+    constructor(x, y, width, height) {
+
+        this.x = x; 
+        this.y = y; 
+        this.width = width; 
+        this.height = height; 
+        this.landed = true; 
+        this.render = function() {
+
+            ctx.strokeStyle = randomColor; 
+            ctx.strokeRect(this.x, this.y, this.width, this.height); 
+        }
+
+    }
+}
+
+function gameLoop() {
+
+    ctx.clearRect(0, 0, game.width, game.height); 
+
+    piece1.render(); 
+
+    document.addEventListener('keydown', movementHandler); 
+}
+
+//auto-scrolling and border hit detection
+function scrollPiece() {
+
+    piece1.y += 3; 
+
+    if (piece1.x < 0) {
+        piece1.x = 0 + 2;
+      }
+      else if (piece1.x + piece1.width > game.width) {
+        piece1.x = game.width - (piece1.width + 2);
+      }
+      else if (piece1.y < 0) {
+        piece1.y = 0 + 2;
+      }
+      else if (piece1.y + piece1.height > game.height) {
+        piece1.y = game.height - (piece1.height + 2);
+      }
+}
+
+// =================MOVEMENT HANDLER======================== //
+function movementHandler(e){
+    console.log('Movement:', e.key); 
+
+    if (e.key === 'a') {
+        piece1.x -= 20; 
+    } else if (e.key === 'd') {
+        piece1.x += 20; 
+    } else if (e.key === 's') {
+        piece1.y += 12; 
+    }
+}
+
+
+
+
+
+
+
+
+
+
