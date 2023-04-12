51d97dcd06243a01bd68c7aceefdf4a2afaa2b1f
Tim Pierce <tim@Tims-MacBook-Pro-4.local>
2023-04-12 12:30:43 -0600

    FIX: Added a function that draws the pieces on init and redraws when they've merged with the array

--- main.js
+++ main.js
@@ -1,42 +1,44 @@
-console.log('JS LOADED'); 
+//===============HTML ELEMENTS======================
 
-//==============VARIABLES==============// 
+const canvas = document.getElementById('game');
+const context = canvas.getContext('2d');
 
-//getting html elements
-const game = document.getElementById('game'); 
-const ctx = game.getContext('2d'); 
+//==============CREATING THE BOARD=================
+context.scale(33, 33);
 
-//game variables 
-// let lives = 5; 
-// let gameTime = 0; 
-// let winCondition = false; 
-// let loseCondition = false; 
-let currentPiece; 
+//function to create the game board
+function createBoard(w, h) {
+    const matrix = [];
+    while (h--) {
+        matrix.push(new Array(w).fill(0));
+    }
+    return matrix;
+}
 
+//init the board at 12 columns and 20 rows based off the function
+const board = createBoard(12, 20);
 
-//=================== CREATING THE BOARD ==================//
-window.addEventListener('DOMContentLoaded', function() {
+//checks the board array to see if there all the spaces in a row are filled up. 
+//If so, a new row is created and filled with 0s. 
+function clearRow() {
+    let rowCount = 1;
+    outer: for (let y = board.length -1; y > 0; --y) {
+        for (let x = 0; x < board[y].length; ++x) {
+            if (board[y][x] === 0) {
+                continue outer;
+            }
+        }
 
-    currentPiece = new gamePiece(player.matrix, player.pos); 
+        const row = board.splice(y, 1)[0].fill(0);
+        board.unshift(row);
+        ++y;
 
-    const runGame = setInterval(gameLoop, 60); 
-    // const pieceFunction = setInterval(scrollPiece, 60); 
-    
+        player.score += rowCount * 100;
+        rowCount *= 2;
+    }
+}
 
-})
-
-game.setAttribute('height', getComputedStyle(game)['height']); 
-game.setAttribute('width', getComputedStyle(game)['width']); 
-
-function createBoard(width, height) {
-    const board = []; 
-    while (height--) {
-        board.push(new Array(width).fill(0)); 
-    } return board; 
-}; 
-
-let board = createBoard(10, 20); 
-
+//checks to see if the pieces landed on the board or not
 function collide(board, player) {
     const m = player.matrix;
     const o = player.pos;
@@ -53,9 +55,6 @@ function collide(board, player) {
 }
 
 
-
-
-// ================ BUILDING THE PIECES =============== // 
 
 //creating the pieces in a matrix
 const tMatrix = [
@@ -100,157 +99,137 @@ const piecesArray = [
     invTMatrix,
 ]; 
 
-console.log(piecesArray); 
+//draws the piece on init
+function drawMatrix(matrix, offset) {
+    matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0 && value !== 1) {
+                context.fillStyle = 'white';
+                context.fillRect(x + offset.x, y + offset.y, 1, 1);
+            } else if (value !== 0 && value !== 2) {
+                context.fillStyle = 'red';
+                context.fillRect(x + offset.x, y + offset.y, 1, 1);
+            }
+        });
+    });
+}
 
-//Creating a random piece index 
-let randomPieceIndex = Math.floor(Math.random() * (piecesArray.length)); 
-let randomPiece = piecesArray[randomPieceIndex]; 
+//redraws pieces on the board when position is recorded 
+function draw() {
+    context.fillStyle = '#000';
+    context.fillRect(0, 0, canvas.width, canvas.height);
 
-//setting up the player variable that will allow for player control
-let player = {
-    pos: {x: 5, y: 5}, 
-    matrix: randomPiece,
-    landed: false,
-    collided: false, 
-    width: 60, 
-    height: 60
-}; 
+    drawMatrix(board, {x: 0, y: 0});
+    drawMatrix(player.matrix, player.pos);
+}
 
-//game piece class to add new pieces 
-class gamePiece {
-    constructor(matrix, position) {
-        this.position = position;  
-        this.matrix = matrix; 
-        this.landed = false; 
-        this.render = function(matrix, offset) { 
-            matrix.forEach((row, y) => {
-                row.forEach((value, x) => {
-                    if (value !== 0 && value !== 2) {
-                        ctx.strokeStyle = 'white';
-                        ctx.strokeRect(x * 30 + offset.x, y * 30 + offset.y, 30, 30);  
-                    } else if (value !== 0 && value !== 1) {
-                        ctx.strokeStyle = 'red';
-                        ctx.strokeRect(x * 30 + offset.x, y * 30 + offset.y, 30, 30); 
-                    } 
-                }); 
-            }); 
+//records position on board if landed 
+function recordPosition(board, player) {
+    player.matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                board[y + player.pos.y][x + player.pos.x] = value;
+            }
+        });
+    });
+}
 
-        }
+
+function playerDrop() {
+    player.pos.y++;
+    if (collide(board, player)) {
+        player.pos.y--;
+        recordPosition(board, player);
+        reset();
+        clearRow();
+        updateScore();
     }
+    dropCounter = 0;
 }
 
 
 
-
-
-
-//===============GAME OPERATIONS=============//
-function gameLoop() {
-
-    ctx.clearRect(0, 0, game.width, game.height);  
-
-    currentPiece.render(player.matrix, player.pos); 
-    console.log('current Piece:', currentPiece); 
-    console.log('player:', player); 
-    scrollPiece(); 
-    
-    if(player.landed) {
-        let newPiece = spawnNewPiece(); 
-        console.log('player landed?', player.landed);
-        newPiece.render(player.matrix, player.pos)
-    }
-
-    document.addEventListener('keydown', movementHandler); 
-
-}
-
-// auto-scrolling and border hit detection
-function scrollPiece() {
-
-    player.pos.y += 3; 
-   
-    if (player.pos.x < 0) {
-        player.pos.x = 0 + 2;
-      }
-      else if (player.pos.x + player.width > game.width) {
-        player.pos.x = game.width - (player.width + 2);
-      }
-      else if (player.pos.y < 0) {
-        player.pos.y = 0 + 2;
-      }
-      else if (player.pos.y + player.height > game.height) {
-        player.pos.y = game.height - (player.height + 2);
-        player.landed = true; 
-        merge(board, player); 
-         
-      }
-
-
-}
-
-// =================MOVEMENT HANDLER======================== //
-function movementHandler(e){
-    console.log('Movement:', e.key); 
-    if (player.landed === false){
-        if (e.key === 'a') {
-        player.pos.x -= 30; 
-        } else if (e.key === 'd') {
-        player.pos.x += 30; 
-        } else if (e.key === 's') {
-        player.pos.y += 12; 
-        } else if (e.key === 'w') {
-            rotate(player.matrix, -1); 
-        }
-        else if (e.key === ' ') {
-            player.pos.y = game.height - (player.height + 2); 
-            player.landed = true; 
-            spawnNewPiece(player.matrix, player.pos); 
-        }
-    }
-};        
-
-//rotating the pieces 90 degrees 
+//function that reverses the matrix and rotates 90 degrees 
 function rotate(matrix) {
-    for (let y = 0; y < matrix.length; y++) {
-        for (let x = 0; x < y; x++) {
+    for (let y = 0; y < matrix.length; ++y) {
+        for (let x = 0; x < y; ++x) {
             [
                 matrix[x][y],
-                matrix[y][x]
-            ] = 
-            [
                 matrix[y][x],
-                matrix[x][y]
-            ]; 
+            ] = [
+                matrix[y][x],
+                matrix[x][y],
+            ];
         }
     }
 
-   
-    matrix.reverse(); 
-   
+    
+        matrix.reverse();
+    
+}
+
+//rotating the player and checking to see if rotating will move it out of the board
+function playerRotate(dir) {
+    const pos = player.pos.x;
+    let offset = 1;
+    rotate(player.matrix, dir);
+    while (collide(board, player)) {
+        player.pos.x += offset;
+        offset = -(offset + (offset > 0 ? 1 : -1));
+        if (offset > player.matrix[0].length) {
+            rotate(player.matrix, -dir);
+            player.pos.x = pos;
+            return;
+        }
+    }
+}
+
+//movement control based on key press. passes through a direction
+document.addEventListener('keydown', e => {
+    if (e.key === 'a') {
+        playerMove(-1);
+    } else if (e.key === 'd') {
+        playerMove(1);
+    } else if (e.key === 's') {
+        playerDrop();
+    } else if (e.key === 'w') {
+        playerRotate(-1);
+    } 
+});
+
+
+//automatic dropping of the piece on a set interval
+let dropCounter = 0;
+let dropInterval = 500;
+
+let lastTime = 0;
+function update(time = 0) {
+    const deltaTime = time - lastTime;
+
+    dropCounter += deltaTime;
+    if (dropCounter > dropInterval) {
+        playerDrop();
+    }
+     
+
+    lastTime = time;
+
+    draw();
+    requestAnimationFrame(update);
 }
 
 
-
-// spawning in a new piece after one has landed 
-function spawnNewPiece() { 
-   player.landed = false; 
-//    player.matrix = piecesArray[Math.floor(Math.random() * (piecesArray.length))]; 
-   player.pos = {x: 5, y: 5}; 
-   return new gamePiece(piecesArray[Math.floor(Math.random() * (piecesArray.length))], player.pos); 
-//    currentPiece.render(player.matrix, player.pos); 
-   
-   
-   
-}; 
+function updateScore() {
+    document.getElementById('score').innerText = player.score;
+}
 
 
+const player = {
+    pos: {x: 0, y: 0},
+    matrix: null,
+    score: 0,
+};
 
-
-
-
-
-
-
-
-
-
+reset();
+updateScore();
+update();
