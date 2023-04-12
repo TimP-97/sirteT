5b242e6ec54c525636b6c6ee93aae169bc35c7d1
Tim Pierce <tim@Tims-MacBook-Pro-4.local>
2023-04-11 18:46:47 -0600

    FEAT: ability to rotate the pieces 90 degrees

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+console.log('JS LOADED'); 
+
+//==============VARIABLES==============// 
+
 //getting html elements
 const game = document.getElementById('game'); 
 const ctx = game.getContext('2d'); 
@@ -7,10 +11,14 @@ const ctx = game.getContext('2d');
 // let gameTime = 0; 
 // let winCondition = false; 
 // let loseCondition = false; 
+let currentPiece; 
+let newPiece; 
 
-//main event listener
+//=================== CREATING THE BOARD ==================//
 window.addEventListener('DOMContentLoaded', function() {
 
+    currentPiece = new gamePiece(player.matrix, player.pos); 
+    newPiece = new gamePiece(player.matrix, player.pos); 
 
     const runGame = setInterval(gameLoop, 60); 
     const pieceFunction = setInterval(scrollPiece, 60); 
@@ -21,33 +29,33 @@ window.addEventListener('DOMContentLoaded', function() {
 game.setAttribute('height', getComputedStyle(game)['height']); 
 game.setAttribute('width', getComputedStyle(game)['width']); 
 
+function createBoard(width, height) {
+    const board = []; 
+    while (height--) {
+        board.push(new Array(width).fill(0)); 
+    } return board; 
+}; 
 
-//random color operations 
-const colorIndex = ['#c92a2a', 'white']; 
-let randomIndex = Math.floor(Math.random() * (colorIndex.length)); 
-let randomColor = colorIndex[randomIndex]; 
+let board = createBoard(10, 20); 
 
-console.log(randomColor); 
+function merge(board, player) {
+    player.matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0 && value !== 2){
+                board[y][x] = value; 
+            } else if (value !== 0 && value !==1){
+                board[y][x] = value; 
+            }
+        }); 
+    }); 
+}
 
-// class tetrimino {
-//     constructor(x, y, width, height) {
 
-//         this.x = x; 
-//         this.y = y; 
-//         this.width = width; 
-//         this.height = height; 
-//         this.landed = false; 
-//         this.alive = true; 
-//         this.render = function() {
 
-//             ctx.strokeStyle = randomColor; 
-//             ctx.strokeRect(this.x, this.y, this.width, this.height); 
-//         }
 
-//     }
-// }
+// ================ BUILDING THE PIECES =============== // 
 
-//creating the pieces 
+//creating the pieces in a matrix
 const tMatrix = [
     [1, 1, 2],
     [0, 2, 0],
@@ -58,19 +66,21 @@ const zigMatrix = [
     [0, 2, 0],
     [1, 2, 0], 
     [1, 0, 0],
+ 
 ]; 
 
 const zagMatrix = [
     [1, 0, 0], 
     [1, 2, 0], 
     [0, 2, 0],
+    
 ]; 
 
 const sqMatrix = [
     [1, 1, 0], 
     [2, 2, 0],
     [0, 0, 0],
-];
+]; 
 
 const invTMatrix = [
     [2, 2, 1], 
@@ -78,28 +88,6 @@ const invTMatrix = [
     [0, 0, 0],
 ]; 
 
-
-
-function render(matrix, offset) {
-    matrix.forEach((row, y) => {
-    row.forEach((value, x) => {
-        if (value !== 0 && value !== 2) {
-            ctx.strokeStyle = 'white';
-            ctx.strokeRect(x * 30 + offset.x, y * 30 + offset.y, 30, 30);  
-        } else if (value !== 0 && value !== 1) {
-            ctx.strokeStyle = 'red';
-            ctx.strokeRect(x * 30 + offset.x, y * 30 + offset.y, 30, 30);  
-        }
-    }); 
-}); 
-// if (player.matrix.randomPiece === zigMatrix || player.matrix.randomPiece === zagMatrix) {
-//     player.width === 60; 
-//     player.height === 90; 
-// } else if (player.matrix.randomPiece == tMatrix) {
-//     player.width == 120; 
-//     player.height == 60; 
-// }
-}; 
 
 
 const piecesArray = [
@@ -112,43 +100,70 @@ const piecesArray = [
 
 console.log(piecesArray); 
 
-//variable that pulls a random piece from an array 
+//Creating a random piece index 
 let randomPieceIndex = Math.floor(Math.random() * (piecesArray.length)); 
 let randomPiece = piecesArray[randomPieceIndex]; 
 
-//setting up the player variable that will let me control the piece later
+//setting up the player variable that will allow for player control
 let player = {
-    pos: {x: 175, y: 5}, 
+    pos: {x: 5, y: 5}, 
     matrix: randomPiece,
     landed: false,
+    collided: false, 
     width: 60, 
     height: 60
 }; 
 
+//game piece class to add new pieces 
+class gamePiece {
+    constructor(matrix, position) {
+        this.position = position;  
+        this.matrix = matrix; 
+        this.landed = false; 
+        this.render = function(matrix, offset) { 
+            matrix.forEach((row, y) => {
+                row.forEach((value, x) => {
+                    if (value !== 0 && value !== 2) {
+                        ctx.strokeStyle = 'white';
+                        ctx.strokeRect(x * 30 + offset.x, y * 30 + offset.y, 30, 30);  
+                    } else if (value !== 0 && value !== 1) {
+                        ctx.strokeStyle = 'red';
+                        ctx.strokeRect(x * 30 + offset.x, y * 30 + offset.y, 30, 30); 
+                    } 
+                }); 
+            }); 
+
+        }
+    }
+}
+
+//creating the game board with a function 
 
 
 
 
+
+//===============GAME OPERATIONS=============//
 function gameLoop() {
 
-    ctx.clearRect(0, 0, game.width, game.height); 
+    ctx.clearRect(0, 0, game.width, game.height);  
 
-    render(player.matrix, player.pos);  
+    currentPiece.render(player.matrix, player.pos); 
+    
+    if(player.landed) {
+        spawnNewPiece(); 
+        newPiece.render(player.matrix, player.pos); 
+    }
 
     document.addEventListener('keydown', movementHandler); 
 
-    spawnNewPiece(); 
 }
-
-
-
 
 // auto-scrolling and border hit detection
 function scrollPiece() {
 
-   
     player.pos.y += 3; 
-
+   
     if (player.pos.x < 0) {
         player.pos.x = 0 + 2;
       }
@@ -161,7 +176,10 @@ function scrollPiece() {
       else if (player.pos.y + player.height > game.height) {
         player.pos.y = game.height - (player.height + 2);
         player.landed = true; 
+        merge(board, player); 
+        console.log('player landed?', player.landed); 
       }
+
 
 }
 
@@ -175,21 +193,51 @@ function movementHandler(e){
         player.pos.x += 30; 
         } else if (e.key === 's') {
         player.pos.y += 12; 
-        } else if (e.key === ' ') {
+        } else if (e.key === 'w') {
+            rotate(player.matrix, -1); 
+        }
+        else if (e.key === ' ') {
             player.pos.y = game.height - (player.height + 2); 
-            player.landed === true; 
-        } return player.landed; 
+            player.landed = true; 
+            spawnNewPiece(player.matrix, player.pos); 
+        }
     }
 };        
+
+//rotating the pieces 90 degrees 
+function rotate(matrix, direction) {
+    for (let y = 0; y < matrix.length; y++) {
+        for (let x = 0; x < y; x++) {
+            [
+                matrix[x][y],
+                matrix[y][x]
+            ] = 
+            [
+                matrix[y][x],
+                matrix[x][y]
+            ]; 
+        }
+    }
+
+    if (direction > 0) {
+        matrix.forEach(row => row.reverse)
+    } else {
+        matrix.reverse(); 
+    }
+}
+
 
 
 // spawning in a new piece after one has landed 
 function spawnNewPiece() {
-    if (player.landed === true) {
-        const newPiece = render(player.matrix, player.pos); 
-        player.landed === false;
-    }
+   player.landed = false; 
+   newPiece = new gamePiece(player.matrix, {x: 180, y:10}); 
+   newPiece.render(); 
+   return true; 
+   
 }; 
+
+
 
 
 
